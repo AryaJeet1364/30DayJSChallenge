1e8f45e5fcd3feffeb3e73fa77c22a1c244d8139
AryaJeet1364 <aryajeet.jha@gmail.com>
2024-07-13 21:00:56 +0530

    First Day

---
+++ Day1.js
@@ -0,0 +1,55 @@
+// Activity 1
+// Task 1
+var num = 8;
+console.log(num);
+
+console.log();
+
+// Task 2
+let str = "What a nice weather!"
+console.log(str);
+
+console.log();
+
+// Activity 2
+// Task 3
+const ans = true;
+console.log(ans);
+
+console.log();
+
+// Activity 3
+// Task 4
+let name = "Rishabh";
+let age = 20;
+let isaboveeighteen = true;
+const users = ["Sneha", "Rohit", "Hitesh"];
+const user = {
+  name: "Hari",
+  age: 16,
+  isaboveeighteen: false,
+};
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isaboveeighteen);
+console.log(typeof users);
+console.log(typeof user);
+
+console.log();
+
+// Activity 4
+// Task 5
+let marks = 92;
+console.log(marks);
+marks = 94;
+console.log(marks);
+
+console.log();
+
+// Activity 5
+// Task 6
+const roll = 78;
+roll = 84;
+console.log(roll);
+
